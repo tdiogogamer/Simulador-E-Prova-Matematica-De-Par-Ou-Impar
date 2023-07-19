695c2c10d2078d8df9d01b8b6da005a58bd05716
tdiogo_gamer <71161033+tdiogogamer@users.noreply.github.com>
2023-07-19 00:28:17 -0300

    Código-fonte do programa utilizado para simular 100.000.000 jogos no intervalo de 0-10

---
+++ 0-10.py
@@ -0,0 +1,41 @@
+import random
+import time
+
+
+# Função que gera 2 números aleatórios entre 0 e 10, soma os dois e verifica se é par ou ímpar
+def play_game():
+    num1 = random.randint(0, 10) # Coloque nos parênteses o intervalo de números para o primeiro jogador
+    num2 = random.randint(0, 10) # Coloque nos parênteses o intervalo de números para o segundo jogador
+    total_sum = num1 + num2
+    return total_sum % 2 == 0
+
+
+# Função que simula os jogos com base no input do usuário e mostra quantos jogos par e ímpar ganharam e as respectivas porcentagens de win rate de cada um
+def main():
+    num_games = int(input("Coloque o número que jogos que você deseja simular: "))
+    even_wins = 0
+    odd_wins = 0
+
+    start_time = time.time()
+
+    for _ in range(num_games):
+        if play_game():
+            even_wins += 1
+        else:
+            odd_wins += 1
+
+    end_time = time.time()
+    total_time = end_time - start_time
+
+    total_wins = even_wins + odd_wins
+    even_percentage = (even_wins / total_wins) * 100
+    odd_percentage = (odd_wins / total_wins) * 100
+
+    print(f"Número de jogos jogados: {num_games}")
+    print(f"Vitórias do par: {even_wins} (Porcentagem: {even_percentage:.2f}%)")
+    print(f"Vitórias do ímpar: {odd_wins} (Porcentagem: {odd_percentage:.2f}%)")
+    print(f"Tempo total: {total_time:.2f} segundos")
+
+
+if __name__ == "__main__":
+    main()
